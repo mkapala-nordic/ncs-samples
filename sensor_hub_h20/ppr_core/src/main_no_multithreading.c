cea5c27c4538fef61daca771a8617845a31481f6
Mateusz Kapala <mateusz.kapala@nordicsemi.no>
2026-02-12 14:56:07 +0100

    no multithreading with manual pm/idle
    Signed-off-by: Mateusz Kapala <mateusz.kapala@nordicsemi.no>

--- sensor_hub_h20/ppr_core/src/main_no_multithreading.c
+++ sensor_hub_h20/ppr_core/src/main_no_multithreading.c
@@ -4,11 +4,13 @@
 #include <zephyr/ipc/ipc_service.h>
 #include <zephyr/drivers/spi.h>
 
+#include <zephyr/pm/pm.h>
+
 /*
  * *** User input ***
  */
 #define SPI_BUFFER_SIZE 35
-#define SPI_READ_INTERVAL_MS 100
+#define SPI_READ_INTERVAL_MS 350
 #define IPC_SEND_INTERVAL 10     // in number of SPI transactions
 
 
@@ -77,6 +79,73 @@ static void ipc_send(void)
 	}
 }
 
+// TIMER TO ENSURE PROPER DELAY
+static volatile bool idle_timer_expired = false;
+static void idle_timer_handler(struct k_timer *timer)
+{
+	idle_timer_expired = true;
+}
+K_TIMER_DEFINE(idle_timer, idle_timer_handler, NULL);
+
+
+#include <hal/nrf_vpr_csr.h>
+
+// from: /ncs/nrf/tests/benchmarks/multicore/common/power_off.c
+static void pm_enter_hibernate(void)
+{
+	csr_write(VPRCSR_NORDIC_VPRNORDICSLEEPCTRL,
+		  VPRCSR_NORDIC_VPRNORDICSLEEPCTRL_SLEEPSTATE_HIBERNATE);
+	nrf_barrier_w();
+	arch_cpu_idle();
+}
+
+// from: /ncs/nrf/tests/benchmarks/multicore/common/power_off.c
+static void pm_exit_hibernate(void)
+{
+	csr_write(VPRCSR_NORDIC_VPRNORDICSLEEPCTRL,
+		  VPRCSR_NORDIC_VPRNORDICSLEEPCTRL_SLEEPSTATE_WAIT);
+
+	/* unlock interrupts after sleep */
+	irq_unlock(MSTATUS_IEN);
+}
+
+// DOES NOT HANDLE DEVICES IN ANY CAPACITY, JUST VPR CORE
+static void idle_custom_pm(void)
+{
+	pm_enter_hibernate();
+	/* Should sleep here */
+	pm_exit_hibernate();
+}
+
+// REQUIRES modifications to pm.c/device_runtime.c to remove all thread-related code
+static void idle_hacked_pm(void)
+{
+	(void) arch_irq_lock();
+	_kernel.idle = z_get_next_timeout_expiry();
+	if (k_is_pre_kernel() || !pm_system_suspend(_kernel.idle)) {
+		k_cpu_idle();
+	}
+}
+
+static void idle(void)
+{
+	if (IS_ENABLED(CONFIG_APP_MANUAL_VPR_PM_CUSTOM)) {
+		idle_custom_pm();
+	} else if (IS_ENABLED(CONFIG_APP_MANUAL_VPR_PM_HACKED_PM)) {
+		idle_hacked_pm();
+	}
+}
+
+static void wait_in_idle_ms(uint32_t sleep_ms)
+{
+	idle_timer_expired = false;
+	k_timer_start(&idle_timer, K_MSEC(sleep_ms), K_NO_WAIT);
+
+	while(!idle_timer_expired) {
+		idle();
+	}
+}
+
 int main(void)
 {
 	// Wait for app core to start
@@ -108,7 +177,13 @@ int main(void)
 			ipc_send();
 		}
 		spi_read_counter++;
-		k_msleep(SPI_READ_INTERVAL_MS);
+
+		if ((IS_ENABLED(CONFIG_APP_MANUAL_VPR_PM_CUSTOM) ||
+		     IS_ENABLED(CONFIG_APP_MANUAL_VPR_PM_HACKED_PM))) {
+			wait_in_idle_ms(SPI_READ_INTERVAL_MS);
+		} else {
+			k_msleep(SPI_READ_INTERVAL_MS);
+		}
 	}
 
 	return 0;

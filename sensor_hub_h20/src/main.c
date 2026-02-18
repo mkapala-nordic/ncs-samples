2555f9a735c9c326c73effe82fc78054980dac18
Mateusz Kapala <mateusz.kapala@nordicsemi.no>
2026-02-18 14:30:26 +0100

    improve adv
    Signed-off-by: Mateusz Kapala <mateusz.kapala@nordicsemi.no>

--- sensor_hub_h20/src/main.c
+++ sensor_hub_h20/src/main.c
@@ -12,9 +12,11 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 /*
  * *** User input ***
  */
-#define START_ADVERTISING 0
+#define START_ADVERTISING 1
 #define ADVERTISING_INTERVAL 1600 //in 625ms steps
-#define IPC_BUFFER_SIZE 350
+#define SPI_SAMPLE_SIZE 35
+#define SPI_SAMPLE_CNT 10
+#define IPC_BUFFER_SIZE (SPI_SAMPLE_SIZE * SPI_SAMPLE_CNT)
 
 
 static uint8_t ipc_data[IPC_BUFFER_SIZE];
@@ -43,13 +45,59 @@ K_WORK_DEFINE(thread_write_flash_id, write_ext_flash);
 /*
  * *** BT settings ***
  */
+
+union manuf_data {
+	struct {
+		uint16_t company_id;
+		uint8_t ipc_xfer_count;
+		uint8_t sample_first_byte[SPI_SAMPLE_CNT];
+	};
+	uint8_t data[sizeof(uint16_t) + sizeof(uint8_t) + (sizeof(uint8_t) * SPI_SAMPLE_CNT)];
+};
+
+static union manuf_data manuf_data = {
+	.company_id = 0x0059, /* Nordic Semiconductor ASA */
+	.ipc_xfer_count = 0,
+	.sample_first_byte = {0},
+};
+
 #define DEVICE_NAME             CONFIG_BT_DEVICE_NAME
 #define DEVICE_NAME_LEN         (sizeof(DEVICE_NAME) - 1)
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
 	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+	BT_DATA(BT_DATA_MANUFACTURER_DATA, manuf_data.data, sizeof(manuf_data.data))
 };
 
+// static const struct bt_data sd[] = {
+// };
+
+static void adv_update_work_handler(struct k_work *work);
+static K_WORK_DEFINE(adv_update_work, adv_update_work_handler);
+static void adv_update_work_handler(struct k_work *work)
+{
+	LOG_INF("Adv update handler");
+
+	// int err = bt_le_adv_update_data(ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+	int err = bt_le_adv_update_data(ad, ARRAY_SIZE(ad), NULL, 0);
+	if (err) {
+		LOG_ERR("Failed to update advertisement data: %d", err);
+	} else {
+		LOG_INF("Advertisement data updated");
+	}
+}
+
+static void process_ipc_data(void)
+{
+	/* Let's call it "data processing".*/
+	manuf_data.ipc_xfer_count = (manuf_data.ipc_xfer_count + 1) % 255;
+	for (int i = 0; i < ARRAY_SIZE(manuf_data.sample_first_byte); i++) {
+		size_t offset = i * SPI_SAMPLE_SIZE;
+		manuf_data.sample_first_byte[i] = ipc_data[offset];
+	}
+
+	k_work_submit(&adv_update_work);
+}
 
 /*
  * *** IPC settings ***
@@ -65,6 +113,8 @@ static void ep_recv(const void *data, size_t len, void *priv)
 	memcpy(ipc_data, data, IPC_BUFFER_SIZE);
 	LOG_HEXDUMP_DBG(ipc_data, IPC_BUFFER_SIZE, "IPC data");
 	k_work_submit(&thread_write_flash_id);
+
+	process_ipc_data();
 }
 static struct ipc_ept_cfg ep_cfg = {
 	.name = "ep0",
